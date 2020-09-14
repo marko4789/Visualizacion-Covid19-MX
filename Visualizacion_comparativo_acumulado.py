9edee4909495772c815f7605b8810cd1badcc803
Marco Ocampos <markos4789@gmail.com>
2020-09-13 19:03:47 -0600

    Comparaciones Casos-Muertes

---
+++ Visualizacion_comparativo_acumulado.py
@@ -0,0 +1,20 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+dataset_casos = pd.read_csv("comparativo_casos_acumulados.csv")
+dataset_muertes = pd.read_csv("comparativo_muertes_acumuladas.csv")
+
+dias = np.arange(244)
+casos = dataset_casos.iloc[:, 1]
+muertes = dataset_muertes.iloc[:, 1]
+
+plt.title("Covid-19 MX - Comparativo casos-muertes acumuladas")
+plt.suptitle("13 de Enero - 12 de Septiembre 2020", fontsize = 10)
+plt.xlabel("Días")
+plt.ylabel("Numero de casos-muertes")
+plt.plot(dias, casos, color = "blue", label = "Número de casos")
+plt.plot(dias, muertes, color = "red", label = "Número de muertes")
+plt.legend()
+#plt.grid(True)
+plt.show()
---
+++ Visualizacion_comparativo_nuevos.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+
+dataset_casos = pd.read_csv("comparativo_casos_nuevos.csv")
+dataset_muertes = pd.read_csv("comparativo_muertes_nuevas.csv")
+
+dias = np.arange(244)
+casos = dataset_casos.iloc[:, 1]
+casos_prom = dataset_casos.iloc[:, 3]
+muertes = dataset_muertes.iloc[:, 1]
+muertes_prom = dataset_muertes.iloc[:, 3]
+
+plt.title("Covid-19 MX - Comparativo casos-muertes diarias")
+plt.suptitle("13 de Enero - 12 de Septiembre 2020", fontsize = 10)
+plt.xlabel("Días")
+plt.ylabel("Numero de casos-muertes")
+plt.plot(dias, casos_prom, color = "green", label = "Promedio diario de casos")
+plt.scatter(dias, casos, color = "blue", label = "Casos nuevos diarios", alpha=0.5)
+plt.plot(dias, muertes_prom, color = "orange", label = "Promedio diario de muertes")
+plt.scatter(dias, muertes, color = "red", label = "Muertes nuevas diarias", alpha=0.5)
+plt.legend()
+#plt.semilogy()
+#plt.grid(True)
+plt.show()
